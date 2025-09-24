eeafb234101de643e1c9980629673866c1a594c3
Neelapu Venkata Naidu <114356828+venkatneelapu@users.noreply.github.com>
2025-09-24 23:47:49 +0530

    Time: 5 ms (56.50%) | Memory: 45.4 MB (21.12%) - LeetSync

--- 2847-find-maximum-number-of-string-pairs/find-maximum-number-of-string-pairs.java
+++ 2847-find-maximum-number-of-string-pairs/find-maximum-number-of-string-pairs.java
@@ -1,11 +1,11 @@
 class Solution {
-    public String reverse(String s){
-        String rev="";
-        for(int i=s.length()-1;i>=0;i--){
-            rev=rev+s.charAt(i);
-        }
-        return rev;
-    }
+    // public String reverse(String s){
+    //     String rev="";
+    //     for(int i=s.length()-1;i>=0;i--){
+    //         rev=rev+s.charAt(i);
+    //     }
+    //     return rev;
+    // }
     public int maximumNumberOfStringPairs(String[] words) {
         int count =0;
         int n=words.length;
@@ -13,7 +13,8 @@ class Solution {
             for(int j=i+1;j<n;j++){
                 String a=words[i];
                 String b=words[j];
-                String reversing=reverse(b);
+                String reversing=new StringBuilder(b).reverse().toString();
+                // String reversing=reverse(b);
                 if(reversing.equals(a)){
                     count++;
                 }
